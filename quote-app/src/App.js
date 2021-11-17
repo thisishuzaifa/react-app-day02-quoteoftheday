297b4bc75d4cf26907c19cf72236a23fd3c53d35
Huzaifa Khalid <huzaifa_khalid97@outlook.com>
2021-11-17 15:25:27 -0800

    Setting up the project
    npm run and ruin my life

---
+++ quote-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import './footer.js';
+import './header.js';
+import './quote.js';
+
+/* Create an “appInfo” JavaScript object with the following key value pairs
+o title -> “Quote of the Day” (string)
+o author -> [Your name] (string)
+o copyright -> [the current year] (number) */
+
+const appInfo = {
+  title: 'Quote of the Day',
+  author: 'Huzaifa',
+  copyright: new Date().getFullYear()
+};
+
+
+
+function App() {
+
+  
+}
+
+export default App;
---
+++ quote-app/src/header.js
@@ -0,0 +1,11 @@
+
+
+const defaultProps = "React App"
+
+function Header(defaultProps) {
+  return (
+    <header className="App-header">
+      <h1>{defaultProps.title}</h1>
+    </header>
+  );
+}
